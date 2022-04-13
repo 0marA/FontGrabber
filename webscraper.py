f49ac6048d31e82446727f0e20440fe7a99641d5
0marA <theengineermachine@gmail.com>
2022-04-12 23:26:01 -0500

    Done!

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
+import keyboard
+import time
 
 dailyGifts = "https://www.creativefabrica.com/daily-gifts/"
 closePopup = "onesignal-slidedown-allow-button"
 
-usernameStr = 'putYourUsernameHere'
-passwordStr = 'putYourPasswordHere'
 driver = webdriver.Firefox()
 driver.get(dailyGifts)
 driver.maximize_window()
-# username = driver.find_element_by_id(i)
-# username.send_keys(usernameStr)
-nextButton = driver.find_element_by_id(closePopup).click()
 
-popup = driver.find_element_by_link_text("Close").click()
+driver.find_element_by_id(closePopup).click()
+driver.find_element_by_link_text("Close").click()
+
 driver.execute_script("window.scrollTo(0, 300)") 
-
 downloadButton = driver.find_element_by_css_selector("span[class='btn-text']").click()
 
+username = driver.find_element_by_name('username')
+username.click()
+username.clear
+username.send_keys("engineermachine@outlook.com")
+
+password = driver.find_element_by_name('password')
+password.click()
+password.clear
+password.send_keys("bruhapple")
+keyboard.press_and_release("enter")
+
+time.sleep(4)
+driver.execute_script("window.scrollTo(0, 300)") 
+downloadButton = driver.find_element_by_css_selector("span[class='btn-text']").click()
