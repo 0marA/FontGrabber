5d79467079e46ac92931527215884d1423336fae
0marA <theengineermachine@gmail.com>
2022-04-13 21:14:12 -0500

    Fixed password

--- webscraper.py
+++ webscraper.py
@@ -1,31 +1,36 @@
-from selenium import webdriver
-import keyboard
 import time
+from selenium import webdriver
+from loginInfo import USERNAME, PASSWORD
+import keyboard
 
-dailyGifts = "https://www.creativefabrica.com/daily-gifts/"
-closePopup = "onesignal-slidedown-allow-button"
+
+DAILY_GIFTS_URL = "https://www.creativefabrica.com/daily-gifts/"
+CLOSE_POPUP_TAG = "onesignal-slidedown-allow-button"
+JOIN_PROMPT_DIV = "promo-upsell-popup-freebie-close"
 
 driver = webdriver.Firefox()
-driver.get(dailyGifts)
+driver.get(DAILY_GIFTS_URL)
 driver.maximize_window()
 
-driver.find_element_by_id(closePopup).click()
+time.sleep(3)
+driver.find_element_by_id(CLOSE_POPUP_TAG).click()
 driver.find_element_by_link_text("Close").click()
 
 driver.execute_script("window.scrollTo(0, 300)") 
 downloadButton = driver.find_element_by_css_selector("span[class='btn-text']").click()
 
-username = driver.find_element_by_name('username')
-username.click()
-username.clear
-username.send_keys("engineermachine@outlook.com")
+usernameBox = driver.find_element_by_name('username')
+usernameBox.click()
+usernameBox.send_keys(USERNAME)
 
-password = driver.find_element_by_name('password')
-password.click()
-password.clear
-password.send_keys("bruhapple")
+passwordBox = driver.find_element_by_name('password')
+passwordBox.click()
+passwordBox.send_keys(PASSWORD)
 keyboard.press_and_release("enter")
 
 time.sleep(4)
-driver.execute_script("window.scrollTo(0, 300)") 
+driver.execute_script("window.scrollTo(0, 300)")
+time.sleep(1)
 downloadButton = driver.find_element_by_css_selector("span[class='btn-text']").click()
+time.sleep(5)
+driver.quit()
